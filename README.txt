d19189747c19cf5aaa52fd69e07c9b728617c4d0
Marcelo Vani <marcellovani@yahoo.co.uk>
2013-09-20 20:54:21 +0100

    Initial commit.

---
+++ README.txt
@@ -0,0 +1,21 @@
+Module: Wysiwyg Smart Paging
+
+Description
+===========
+The Wysiwyg Smart Paging module provides UI for multi-page metatags.
+Compatible with: Metatags module.
+
+Installation
+============
+Copy the module directory in to your Drupal:
+/sites/all/modules directory as usual.
+
+Configuration
+=============
+Visit /admin/config/content/wysiwyg
+or via menus: Configuration > Content Authoring > Wysiwyg profiles
+
+Edit the configuration for each Input format, i.e. Filtered HTML
+/admin/config/content/wysiwyg/profile/filtered_html/edit
+
+Tick the option: Wysiwyg Smart Paging
---
+++ plugins/wysiwyg_smart_paging/wysiwyg_smart_paging.js
@@ -0,0 +1,207 @@
+(function ($) {
+  Drupal.wysiwyg.plugins['wysiwyg_smart_paging'] = {
+    
+    /**
+     * Return whether the passed node belongs to this plugin.
+     */
+    isNode: function(node) {
+      return is_wysiwyg_smart_paging = $(node).is('.wysiwyg-sp-img');
+    },
+
+    /**
+     * Execute the button.
+     */
+    invoke: function(data, settings, instanceId) {
+      // Display the Add form.
+      if (data.content == "") {
+        // Insert a Page Break.
+        var tmp_settings = {};
+        tmp_settings.path = settings.smart_paging_path + "/plugins/wysiwyg/smart_paging";
+        if (typeof Drupal.wysiwyg.plugins['smart_paging'] != 'undefined') {
+          Drupal.wysiwyg.plugins['smart_paging'].invoke(data, tmp_settings, instanceId);
+        }
+
+        // Alocate a new id.
+        settings.data_id = 1;
+        var regex = new RegExp(settings.metatag_markup['prefix'], 'gi');
+        var matches = Drupal.wysiwyg.instances[instanceId].getContent().match(regex);
+        
+        if (matches != null) {
+          settings.data_id = matches.length + 1;
+        }
+        settings.placeholder = this._getImgPlaceholder(settings);
+        settings.mode = "Add";
+      }
+      // Display the Edit form.
+      else {
+        var regexp = new RegExp('<img.*?alt="(\\d+)".*?>', 'gi');
+        var data_id = data.content.replace(regexp, '$1');
+        settings.data_id = parseInt(data_id);
+        settings.mode = "Edit";
+      }
+      // Show the input form.
+      this.show_popup(settings, instanceId);
+    },
+
+    /**
+     * Replace Metatags with image.
+     */
+    attach: function(content, settings, instanceId) {
+      // Find all content from metatags markup.
+      var regexp = new RegExp(settings.metatag_markup['prefix'] + '(.*?)' + settings.metatag_markup['suffix'], 'gi');
+      var data_id_array = content.match(regexp);
+      if (data_id_array != null) {
+        for (var i = 0; i < data_id_array.length; i++) {
+          var metatag_data = JSON.parse(data_id_array[i].replace(regexp, '$1', 'gi'));
+          settings.data_id = metatag_data.data_id;
+          
+          // Store data.
+          Drupal.wysiwyg.plugins['wysiwyg_smart_paging'].data[settings.data_id] = metatag_data;
+          
+          // Replace markup with image.
+          content = content.replace(data_id_array[i], this._getImgPlaceholder(settings));
+        }
+      }
+      return content;
+    },
+
+    /**
+     * Replace images with Metatags.
+     */
+    detach: function(content, settings, instanceId) {
+      // Find all Images.
+      var regexp = new RegExp(
+        settings.icon_markup['prefix'] + '.*?alt="(\\d+)".*?' +
+        settings.icon_markup['suffix'], 'gi');
+      var data_id_array = content.match(regexp);
+      if (data_id_array != null) {
+        for (var i = 0; i < data_id_array.length; i++) {
+          var data_id = data_id_array[i].replace(regexp, '$1', 'gi');
+          settings.data_id = data_id;
+          
+          // Generate markup using data from storage.
+          var markup = JSON.stringify(Drupal.wysiwyg.plugins['wysiwyg_smart_paging'].data[settings.data_id]);
+          
+          // Replace image with markup.
+          content = content.replace(data_id_array[i], 
+            settings.metatag_markup['prefix'] + 
+            markup + settings.metatag_markup['suffix']);
+        }
+      }
+      return content;
+    },
+
+    /**
+     * Shows the Form.
+     */
+    show_popup: function(settings, instanceId) {
+      if (typeof settings.data_id == 'string') {
+      	return;
+      }
+
+      // Check if the Form is not yet on the DOM.
+      if ($('.wysiwyg-sp-popup').length == 0) {
+        // Print the form on the page.
+        // Normalize the field names.
+        form = settings.form_markup.replace(new RegExp('metatags\\[([\\w|:|-]+)\\]\\[value\\]', 'gi'), 'meta_name_$1_content');
+        // Fix robots tags manually. @TODO: use RegExp here too.
+        //form = form.replace("robots[", "robots:");
+        //form = form.replace("]", "");
+        jQuery("body").append(form);
+
+        if (settings.mode == "Edit") {
+          // Fill in the form values with json data.
+
+          var metatags = Drupal.wysiwyg.plugins['wysiwyg_smart_paging'].data[settings.data_id];
+
+          jQuery.each(metatags, function(name, value) {
+            jQuery('.wysiwyg-sp-popup [name=' + name + ']').val(value);
+          });
+        }
+      }
+
+      // Display popup centered on screen.
+      jQuery(".wysiwyg-sp-popup").center().show(function() {
+
+        // Listeners for buttons.
+        jQuery(".wysiwyg-sp-popup .insert").click(function() {
+          // Get key/values from the form.
+          var metatags = Drupal.wysiwyg.plugins['wysiwyg_smart_paging']._getFormValues(settings);
+
+          // Store the metatags.
+          Drupal.wysiwyg.plugins['wysiwyg_smart_paging'].data[settings.data_id] = metatags;
+
+          // Insert the placeholder image.
+          if (settings.mode == "Add") {
+            Drupal.wysiwyg.instances[instanceId].insert(settings.placeholder);
+          }
+          
+          // Close popup.
+          jQuery(".wysiwyg-sp-popup").remove();
+        });
+        jQuery(".wysiwyg-sp-popup .cancel").click(function() {
+          jQuery(".wysiwyg-sp-popup").remove();
+        });
+        // Catch keyboard events.
+        jQuery(document).keydown(function(e) {
+          // Esc key pressed.
+          if (e.keyCode == 27) {
+            jQuery(".wysiwyg-sp-popup").remove();
+          }
+        });
+        jQuery(".wysiwyg-sp-popup *:input[type!=hidden]:first").focus();
+      });
+    },
+
+    /**
+     * Helper function to return a HTML placeholder.
+     */
+    _getImgPlaceholder: function (settings) {
+      return settings.icon_markup['prefix'] +
+        settings.icon_markup['src'].replace('alt=""', 'alt="' +
+        settings.data_id + '"') +
+        settings.icon_markup['suffix'];
+    },
+            
+    /**
+     * Helper function to return all values from the form.
+     */
+    _getFormValues: function(settings) {
+      var metatags = {};
+      metatags["data_id"] = settings.data_id;
+
+      // Get values from the form. Only get values from input type: text, textarea
+      jQuery('.wysiwyg-sp-popup *').filter('input[type=text],textarea').each(function(key, value) {
+
+        // Ignore fields that were not normalized when building the form, they wil have [] symbols.
+        if (this.name.indexOf("[") == -1 && this.name.indexOf("]") == -1) {
+          // Only override tags that contain values.
+          if (this.value != "") {
+            metatags[this.name] = this.value;
+          }
+        }
+      });
+      return metatags;
+    },
+  };
+
+  // Storage.
+  Drupal.wysiwyg.plugins['wysiwyg_smart_paging'].data = {};
+  
+})(jQuery);
+
+/**
+ * Center the element on the screen.
+ */
+if (!jQuery.fn.center) {
+  jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, ((jQuery(window).height() -
+      jQuery(this).outerHeight()) / 2) +
+      jQuery(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, ((jQuery(window).width() -
+      jQuery(this).outerWidth()) / 2) +
+      jQuery(window).scrollLeft()) + "px");
+    return this;
+  }
+}
